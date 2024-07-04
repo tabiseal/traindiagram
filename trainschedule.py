34297b4381e04dee55b2c8aeb99e76ee489bcf48
tabiseal <115387977+tabiseal@users.noreply.github.com>
2024-07-05 02:04:16 +0900

    Update trainschedule.py

--- trainschedule.py
+++ trainschedule.py
@@ -1,68 +1,41 @@
 import matplotlib.pyplot as plt
+
 class TrainSchedule:
-    def __init__(self):
-        self.train_lines = {}
+    def __init__(self, train_lines, stations):
+        self.train_lines = {line: {} for line in train_lines}
+        self.stations = stations
         self.fig, self.ax = plt.subplots()
         self.ax.set_xlim(0, 60)
         plt.xticks(range(0, 61, 5))
+        plt.yticks(range(1, len(stations) + 1), stations)
         plt.grid(True)
-
-    def add_train_line(self, line_name):
-        if line_name not in self.train_lines:
-            self.train_lines[line_name] = {
-                "stations": {},
-                "departure_times": {},
-                "station_positions": {}
-            }
-        self.update_y_axis()
-
-    def add_station(self, line_name, station_name, station_time):
-        line = self.train_lines.get(line_name)
-        if line:
-            if station_name not in line["stations"]:
-                position = len(line["stations"]) + 1
-                line["station_positions"][station_name] = position
-                line["stations"][station_name] = station_time
-                line["departure_times"][station_name] = station_time
-                # 自动连接前一个站点
-                if position > 1:
-                    prev_station = list(line["stations"].keys())[-2]
-                    self.connect_stations(line_name, prev_station, station_name)
-            self.update_y_axis()
-
-    def connect_stations(self, line_name, station1, station2):
-        line = self.train_lines.get(line_name)
-        if line and station1 in line["departure_times"] and station2 in line["stations"]:
-            y1, y2 = line["station_positions"][station1], line["station_positions"][station2]
-            x1, x2 = line["departure_times"][station1], line["stations"][station2]
-            self.ax.plot([x1, x2], [y1, y2], 'b-')
-
-    def add_stop_duration(self, line_name, station, duration):
-        line = self.train_lines.get(line_name)
-        if line and station in line["stations"]:
-            y_position = line["station_positions"][station]
-            x_start = line["stations"][station]
-            x_end = x_start + duration
-            self.ax.plot([x_start, x_end], [y_position, y_position], 'r-')
-            line["departure_times"][station] = x_end
-
-    def update_y_axis(self):
-        all_station_names = set()
-        for line in self.train_lines.values():
-            all_station_names.update(line["station_positions"].keys())
-        sorted_station_names = sorted(all_station_names)
-        station_position_mapping = {name: i + 1 for i, name in enumerate(sorted_station_names)}
-        for line in self.train_lines.values():
-            for station_name in line["station_positions"].keys():
-                line["station_positions"][station_name] = station_position_mapping[station_name]
-        if sorted_station_names:
-            self.ax.set_ylim(0, len(sorted_station_names) + 1)
-            plt.yticks(range(1, len(sorted_station_names) + 1), sorted_station_names)
-        else:
-            self.ax.set_ylim(0, 1)
-            plt.yticks([])
         self.ax.invert_yaxis()
+
+    def add_station_time(self, line_name, station_name, arrival_time, departure_time=None):
+        line = self.train_lines.get(line_name)
+        if line is not None:
+            station_index = self.stations.index(station_name) + 1
+            line[station_name] = (arrival_time, departure_time)
+            if departure_time:
+                # Draw stop duration with red line
+                self.ax.plot([arrival_time, departure_time], [station_index, station_index], 'r-')
+            self.connect_stations(line_name)
+
+    def connect_stations(self, line_name):
+        line = self.train_lines.get(line_name)
+        sorted_stations = sorted((station, times) for station, times in line.items() if times)
+        last_time = None  # To store the last time we need to connect from
+        for station, times in sorted_stations:
+            arrival_time, departure_time = times
+            current_time = departure_time if departure_time else arrival_time
+            if last_time is not None:
+                # Draw travel line with blue line
+                self.ax.plot([last_time, arrival_time], 
+                             [self.stations.index(previous_station) + 1, self.stations.index(station) + 1], 'b-')
+            last_time = current_time
+            previous_station = station
 
     def show(self):
         plt.show()
+
 
